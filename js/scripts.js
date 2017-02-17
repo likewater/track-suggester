b2270297bad9db59f37967c3d1b34c6864f61fed
Likewater <lakebayomackway@gmail.com>
2017-02-17 13:19:47 -0800

    Add more js branching and added nextSteps div to index.html.

--- js/scripts.js
+++ js/scripts.js
@@ -2,16 +2,24 @@ $(document).ready(function() {
   $("form#questions").submit(function() {
     event.preventDefault();
     var preference = $("input:radio[name=preference]:checked").val();
+    var coding = $("input:radio[name=coding]:checked").val();
+    var skill = $("input:radio[name=skill]:checked").val();
+    var personality = $("input:checkbox[name=personality]:checked").val();
     var readyToLearn = $("input:radio[name=commit]:checked").val();
 
     if (preference === "designing"){
-    $("#designTrack").show();
-  }
+      $("#designTrack").show();
+    } else if (preference === "coding"){
+      $("#javaTrack").show();
+    } else if (readyToLearn === "yes"){
+      $("#nextStep").show();
+    } else if (readyToLearn === "no"){
+      $("#moreInfo").show();
+    }
 
 
 
-    if (readyToLearn === "no"){
-    $("#moreInfo").show();
-  }
+
+
   });
 });
