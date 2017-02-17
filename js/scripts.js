15a11105ac5c90047d25b29d5cb6384e7576cbdd
Likewater <lakebayomackway@gmail.com>
2017-02-17 12:17:53 -0800

    Add CSS for jumbotron, img for jumbotron, formatted index.html.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,14 @@
 $(document).ready(function() {
   $("form#questions").submit(function() {
     event.preventDefault();
-
-
+    var preference = $("input:radio[name=preference]:checked").val();
     var readyToLearn = $("input:radio[name=commit]:checked").val();
+
+    if (preference === "designing"){
+    $("#designTrack").show();
+  }
+
+
 
     if (readyToLearn === "no"){
     $("#moreInfo").show();
